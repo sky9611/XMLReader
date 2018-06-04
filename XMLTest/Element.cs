92dce72e7d4ffccfc4840e06658829fde8682ac0
Unknown <collinbeyond@gmail.com>
2018-06-04 15:40:57 +0200

    read elements (no debug ver.)

--- XMLTest/Element.cs
+++ XMLTest/Element.cs
@@ -6,17 +6,48 @@ namespace XMLTest
 {
     public class Element
     {
-        string identifier_;
-        string type_;
-        string name_;
-        string documentation_;
-        Property[] properties_;
+        private	string identifier_;
+        private string type_;
+        private string name_;
+        private string documentation_;
+        private List<Property> properties_;
+		
+		public string Identifier_
+		{
+			get { return identifier_; }
+			set { identifier_ = value; }
+		}
+		
+		public string Type_
+		{
+			get { return type_; }
+			set { type_ = value; }
+		}
+		
+		public string Name_
+		{
+			get { return name_; }
+			set { name_ = value; }
+		}
+
+		public string Documentation_
+		{
+			get { return documentation_; }
+			set { documentation_ = value; }
+		}
+
+		public List<Property> Properties_
+		{
+			get { return properties_; }
+			set { properties_ = value; }
+		}
+
 
         public Element()
         {
         }
 
-        public Element(string identifier_, string type_, string name_, string documentation_, Property[] properties_)
+        public Element(string identifier_, string type_, string name_, string documentation_, List<Property> properties_)
         {
             this.identifier_ = identifier_;
             this.type_ = type_;
--- XMLTest/Program.cs
+++ XMLTest/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
 
 namespace XMLTest
 {
     class Program
     {
-        XmlDocument doc = new XmlDocument();
-        List<Element> elements = new List<Element>(); 
+        XDocument doc = new XmlDocument();
+        List<Element> list_element = new List<Element>(); 
+		Dictionary<string,string> property_definition_map = new Dictionary<string,string>();
         public void ReadXml(string path)
         {
             doc.Load(path);
         }
+		
+		public void getPropertyDefinition()
+		{
+			if (doc == null)
+            {
+                Console.WriteLine("XML not loaded yet");
+            }
+            else
+            {
+				doc.DescendantsAndSelf("propertyDefinition").Select(x=>
+					property_definition_map.add(x.Element("propertyDefinition").Attribute("identifier").Value, x.Element("name").Value));
+			}
+		}
 
         public void DataIn()
         {
@@ -21,20 +36,41 @@ namespace XMLTest
             }
             else
             {
-                XmlNode xml_node = doc.SelectSingleNode("elements");
-                XmlNodeList xml_node_list = xml_node.ChildNodes;
-                foreach (XmlNode xn in xml_node_list)
-                {
-                    Element eleemnt = new Element();
-                }
+                list_element=doc.DescendantsAndSelf("element").Select(x=>
+				new Element
+				{
+					identifier_=x.Element("Element").Attribute("identifier").Value,
+					type_=x.Element("Element").Attribute("type").Value,
+					name_= (x.Element("name")!=null) ? x.Element("name").Value : "",
+					documentation_= (x.Elements("documentation")!=null) ? x.Elements("documentation").Value : "",
+					properties_ = (x.Elements("properties")!=null) ? x.DescendantsAndSelf("property").Select(y=>
+						string prop_name;
+						property_definition_map.TryGetValue(y.Elements("property").Attribute("propertyDefinitionRef").Value,prop_name);
+						new Property
+						{
+							name = prop_name;
+							value = y.Element("Value").Value;
+						}).ToList<Property>() : new List<Property>()
+				).ToList<Element>();
             }
         }
+		
+		public void printElements()
+		{
+			foreach (element in list_element)
+			{
+				Console.WriteLine(element);
+			}
+		}		
 
         static void Main(string[] args)
         {
             Program program = new Program();
             program.ReadXml("TOB");
-            Console.WriteLine("Hello World!");
+			program.getPropertyDefinition();
+			program.DataIn();
+			program.printElements();
+            //Console.WriteLine("Hello World!");
         }
     }
 }
